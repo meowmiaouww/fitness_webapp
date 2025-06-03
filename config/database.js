f094389ac487a7dec3a4a27d9dd47b0b60db1448
Anna Kudymova <100238692+meowmiaouww@users.noreply.github.com>
2025-06-03 06:49:40 +0500

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'neko02'
+    },
+    'database': 'wellness_webapp',
+    'users_table': 'users'
+};
